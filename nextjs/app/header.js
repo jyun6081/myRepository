4cd0c4dba8fae1ab64f17d9c394b9f4be41df0f6
jyun <jyun6081@gmail.com>
2023-05-07 01:56:10 +0900

    nextjs_2

--- nextjs/app/header.js
+++ nextjs/app/header.js
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import DarkModeToggleButton from "./dark-mode-toggle-button"
 
 export default function(){
     return(
@@ -16,11 +17,7 @@ export default function(){
                     <Link className="mr-5 hover:text-gray-900" href="/projects">Project</Link>
                     <a className="mr-5 hover:text-gray-900" href="https://open.kakao.com/o/s6WF32if">Kakaotalk으로 연락</a>
                     </nav>
-                    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                    </svg>
-                    </button>
+                    <DarkModeToggleButton/>
                 </div>
             </header>
         </>
--- nextjs/app/layout.js
+++ nextjs/app/layout.js
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from './header';
 import Footer from './footer';
+import Providers from './providers';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -12,12 +13,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
-      <body className={inter.className + " g_font"}>
-        <Header/>
-        <divs>
-          {children}
-        </divs>
-        <Footer/>
+      <body className={inter.className + " g_font bg-primary"}>
+        <Providers>
+          <Header/>
+          <divs>
+            {children}
+          </divs>
+          <Footer/>
+        </Providers>
       </body>
     </html>
   )
